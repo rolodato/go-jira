627fc1cb6d334b61f5650f5b3b46681ce231470b
Rodrigo López Dato <rodrigo.lopez@toggl.com>
2021-03-30 10:24:45 +0300

    feat: add UntranslatedName property to IssueType

--- issue.go
+++ issue.go
@@ -231,13 +231,14 @@ type IssueRenderedFields struct {
 // IssueType represents a type of a Jira issue.
 // Typical types are "Request", "Bug", "Story", ...
 type IssueType struct {
-	Self        string `json:"self,omitempty" structs:"self,omitempty"`
-	ID          string `json:"id,omitempty" structs:"id,omitempty"`
-	Description string `json:"description,omitempty" structs:"description,omitempty"`
-	IconURL     string `json:"iconUrl,omitempty" structs:"iconUrl,omitempty"`
-	Name        string `json:"name,omitempty" structs:"name,omitempty"`
-	Subtask     bool   `json:"subtask,omitempty" structs:"subtask,omitempty"`
-	AvatarID    int    `json:"avatarId,omitempty" structs:"avatarId,omitempty"`
+	Self             string `json:"self,omitempty" structs:"self,omitempty"`
+	ID               string `json:"id,omitempty" structs:"id,omitempty"`
+	Description      string `json:"description,omitempty" structs:"description,omitempty"`
+	IconURL          string `json:"iconUrl,omitempty" structs:"iconUrl,omitempty"`
+	Name             string `json:"name,omitempty" structs:"name,omitempty"`
+	UntranslatedName string `json:"untranslatedName,omitempty" structs:"name,omitempty"`
+	Subtask          bool   `json:"subtask,omitempty" structs:"subtask,omitempty"`
+	AvatarID         int    `json:"avatarId,omitempty" structs:"avatarId,omitempty"`
 }
 
 // Watches represents a type of how many and which user are "observing" a Jira issue to track the status / updates.
